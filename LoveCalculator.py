84c174e755e10873d1095c894cc4f1e0880dee7f
Yanru Lu <yanru.lu@hu-berlin.de>
2019-12-14 21:49:13 +0100

    Delete unnecessary parts and add some comments

--- LoveCalculator.py
+++ LoveCalculator.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  8 12:56:08 2019
 
-@author: luzi
-"""
 import re
 import nltk
 #import nltk.corpus.reader.tagged as tagged
@@ -14,24 +10,10 @@ import dill
 import util
 from tensorflow.keras.models import load_model
 
+# load sentiment analysis model
 vocabulary = dill.load(open("vocabulary.dat", "rb"))
 model = load_model("classifier.h5")
 
-#def get_username():
-#    filename = "chatty_username.txt"
-#    if exists(filename):
-#        with open(filename, 'r') as f:
-#            return f.read().strip()
-#    else: 
-#        username = input("Hallo, mein Name ist Chatty. Wie heißt du?\n")
-#        with open(filename, 'w') as f:
-#            f.write(username)
-#        return username
-    
-with open( "tiger.model", "rb" ) as f:
-    pos_model = dill.load(f)
-#username = get_username()
-#print("Hallo " + username)
 print("Herzlich willkommen, du bist hier genau richtig. Ich bin der schlauste LoveCalculator in der Welt und kann die Zukunft von dir und deinem Partner/deiner Partnerin vorhersagen.\n")
 user = "neu"
 
@@ -57,7 +39,7 @@ while user != "":
     # negative training data?
     # every positive word count as a positive point and every negative word as negative one
     #love_partner =
-    
+
     #nach der Beschreibung vom Partner
     user = input("Beschreib bitte deine persönliche Empfindung von eurer Beziehung: \n")
     # sentiment analysis (similar to imdb)
@@ -69,7 +51,7 @@ while user != "":
         sum_relationship += y[i]
     love_relationship = sum_relationship/len(y)
     print(love_relationship)
-    
+
     # results of calculation
     love_index = love_name_value*0.1 + love_partner*0.3 + love_relationship*100*0.6
     if (love_index>=1 and love_index<=20):
@@ -82,7 +64,7 @@ while user != "":
         print("Bei euch beiden sprühen die Funken.")
     elif(love_index>=81 and love_index<=100):
         print("Das ist wahre Liebe.")
-    
+
     #nach beiden Angaben
     user = input("Möchtest du Vorschläge von mir bekommen?") # Rückgabe je nach Ergebnis von der Berechnung oben
     if (re.search("((j|J)a )|((g|G)ern)", user)):
@@ -90,6 +72,5 @@ while user != "":
         user = input("Tipp 'neu' ein, um einen neuen Test zu starten. Ansonsten drück die Enter-Taste, um den Test zu beenden.\n")
     else:
         break
-    
+
 print("Vielen Dank für deinen Besuch und auf Wiedersehen!")
-    
